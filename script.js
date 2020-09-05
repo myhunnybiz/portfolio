d45fddff7e457781b7de8e5364652dd60326840a
Jorge <51365285+myhunnybiz@users.noreply.github.com>
2020-09-06 04:48:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const toggle = document.getElementById('toggle');
 
+const nav = document.getElementById('main-nav');
+
+
 toggle.addEventListener('click', () =>
 document.body.classList.toggle('show-nav')
+);
+
+nav.addEventListener('click', () => document.body.classList.remove('show-nav')
 );
